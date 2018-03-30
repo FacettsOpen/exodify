a548efeb84a5b16101d4b80b0d0c3e0bff6a6ae9
BillCarsonFr <bill.carson@valrsoft.com>
2018-03-30 11:38:23 +0200

    Fix / api Call in background.js not using auth token
    + cleaning

--- sources/background.js
+++ sources/background.js
@@ -1,40 +1,9 @@
-//TODO -> Create exodus-rest.js for reuse across scripts
-/*
-* Fetches from exodus privacy and returns -1 if unknown, or >0 number of trackers.
-*/
-function fetchNbTrackersFor(appID,callback,err) {
-  var xmlHttp = new XMLHttpRequest();
-  
-  function reqListener () {
-    // console.log(this.responseText);
-    try {
-      var json = JSON.parse(xmlHttp.responseText);
-      if (json[appID] && json[appID]['reports']) {
-        const nbReports = json[appID]['reports'].length;
-        const lastReport = json[appID]['reports'][nbReports - 1];
-        const nbTrackers = lastReport.trackers.length
-        callback(appID,nbTrackers)
-      } else {
-        callback(appID,-1)
-      }
-    } catch(e) {
-    	err()
-    }
-  }
 
-  xmlHttp.addEventListener("load", reqListener);
-  xmlHttp.open( "GET", 'https://reports.exodus-privacy.eu.org/api/search/'+appID );
-  xmlHttp.send( null );
-  //console.log('Response for '+ appID +'is ' + xmlHttp.responseText)
-  
-}
 function getParameterByName(query, name) {
   var match = new RegExp('[?&]' + name + '=([^&]*)').exec(query);
   return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
 }
 
-//var tabMem = {}
-// var currentTabId = ''
 
 browser.runtime.onMessage.addListener(function(message,sender) {
   //console.log("MMMMMM BACKGROUND" + JSON.stringify(message))
@@ -43,7 +12,9 @@ browser.runtime.onMessage.addListener(function(message,sender) {
     var appId = message.appId
     var nb = message.nbTrackers
     // tabMem[tabId] =  message.nbTrackers
-    fetchNbTrackersFor(appId,function(id,nb){
+    $ep.fetchLatestReportFor(appId,function(id,name,report){
+        const nb = report ? report.trackers.length : -1
+    // fetchNbTrackersFor(appId,function(id,nb){
       //check that tab url still correct (async)
         browser.browserAction.setBadgeBackgroundColor({color:'#224955'/*,tabId: tab.id*/})
         if (nb == -1) {
@@ -76,22 +47,7 @@ browser.runtime.onMessage.addListener(function(message,sender) {
   }) 
 
 browser.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-  //console.log('F.. tab change man! ' + JSON.stringify(changeInfo))
-  //var tabMem = {}
-  // tabMem[tabId.tabId] = undefined
   browser.browserAction.setBadgeText({text:'',tabId: tabId.tabId})
 })
 
-//browser.tabs.onActivated.addListener((tabId, changeInfo, tab) => {
-  //console.log('F.. tab activated change man!')
-  // var currentTabId = tabId.tabId
-  // if (tabMem[currentTabId] != undefined) {
-  //   if (tabMem[currentTabId] == -1) {
-  //     browser.browserAction.setBadgeText({text:''})
-  //   } else {
-  //     browser.browserAction.setBadgeText({text:'' + tabMem[currentTabId]})
-  //   }
-  // } else {
-  //  browser.browserAction.setBadgeText({text:''})
-  // }
-// })
+
